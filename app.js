c8781bef7992d86e5007e864d02d2f9ef65bccb8
Lucian Bellini <lucianbellini1@gmail.com>
2023-08-03 16:27:57 -0300

    Enableing json use

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const app = express();
+app.use(express.json());
 
 //Open route - Public route
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Hello World! :D" });
 });
 
+//Register user
+app.post("/auth/register", async (req, res) => {
+  const { username, email, password, confpassword } = req.body;
+
+  //Validations
+  if (!username) {
+    res.status(422).json({ message: "Username is mandatory" });
+  }
+});
+
+//Connecting to database
 const connectToDb = () => {
   mongoose
     .connect(process.env.DB_URI, {
